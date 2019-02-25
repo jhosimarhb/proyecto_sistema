2608d75f7e785d6b70f6098e3f7eaefa3191a223
jhosimarhb <josho1128@gmail.com>
2019-02-25 12:11:20 -0600

    model1
    model

---
+++ model/eliminar.php
@@ -0,0 +1,62 @@
+<?php
+
+	# conectare la base de datos
+    $con=@mysqli_connect('localhost', 'root', '', 'preguntas');
+    if(!$con){
+        die("imposible conectarse: ".mysqli_error($con));
+    }
+    if (@mysqli_connect_errno()) {
+        die("Connect failed: ".mysqli_connect_errno()." : ". mysqli_connect_error());
+    }
+	/*Inicia validacion del lado del servidor*/
+	 if (empty($_POST['id'])){
+			$errors[] = "ID vacío";
+		}   else if (
+			!empty($_POST['id']) 
+			
+		){
+ 
+		// escaping, additionally removing everything that could be (html/javascript-) code
+		$id=intval($_POST['id']);
+		
+		$sql="DELETE FROM pregunta WHERE id='".$id."'";
+		$query_delete = mysqli_query($con,$sql);
+			if ($query_delete){
+				$messages[] = "Los datos han sido eliminados satisfactoriamente.";
+			} else{
+				$errors []= "Lo siento algo ha salido mal intenta nuevamente.";
+			}
+		} else {
+			$errors []= "Error desconocido.";
+		}
+		
+		if (isset($errors)){
+			
+			?>
+			<div class="alert alert-danger" role="alert">
+				<button type="button" class="close" data-dismiss="alert">&times;</button>
+					<strong>Error!</strong> 
+					<?php
+						foreach ($errors as $error) {
+								echo $error;
+							}
+						?>
+			</div>
+			<?php
+			}
+			if (isset($messages)){
+				
+				?>
+				<div class="alert alert-success" role="alert">
+						<button type="button" class="close" data-dismiss="alert">&times;</button>
+						<strong>¡Bien hecho!</strong>
+						<?php
+							foreach ($messages as $message) {
+									echo $message;
+								}
+							?>
+				</div>
+				<?php
+			}
+ 
+?>
